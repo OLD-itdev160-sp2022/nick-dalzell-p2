f0bd60efa69ae346833013e757d3ef0ab4475ead
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-05-01 20:25:31 -0500

    Finished Project 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,64 @@
+//queries for selectors for image slider
+let sliderImages = document.querySelectorAll(".slide"),
+arrowLeft = document.querySelector("#arrow-left"),
+arrowRight = document.querySelector("#arrow-right"),
+current = 0;
+
+//clear all images
+function reset() {
+for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+}
+}
+
+//initial slide
+function startSlide() {
+reset();
+sliderImages[0].style.display = "block";
+}
+
+//show previous image
+function slideLeft() {
+reset();
+sliderImages[current - 1].style.display = "block";
+current--;
+}
+
+//show next image
+function slideRight() {
+reset();
+sliderImages[current + 1].style.display = "block";
+current++;
+}
+
+//left arrow click event handler
+arrowLeft.addEventListener("click", function () {
+if (current === 0) {
+    current = sliderImages.length;
+}
+slideLeft();
+});
+
+//right arrow click event handler
+arrowRight.addEventListener("click", function () {
+if (current === sliderImages.length - 1) {
+    current = -1;
+}
+slideRight();
+});
+
+startSlide();
+
 //create array of quotes
 var quotes = [
-    'Forefathers one and all! Bear witness! - Godrick the Grafted',
-    'Join the Serpent King as Family. Together, we shall devour the very Gods! - Rykard, Lord of Blasphemy',
-    'I could never betray her, no matter what happens. Ranni, she needs me... - Blaidd, the Half Wolf',
-    'I am Malenia, blade of Miquella. And I have never known defeat. - Malenia, blade of Miquella',
-    'I was able to live as my own person, if only in passing. -Millicent',
-    'I have given thee courtesy enough. Now I fight as Hoarah Loux, WARRIOR! -Hoarah Loux',
-    'Well then, shall we? My dear consort eternal. -Ranni the Witch',
-    'Have it writ upon thy meagre grave: Felled by Morgott, last of all kings! -Morgott the Omen King'
+    '"Forefathers one and all! Bear witness!" - Godrick the Grafted',
+    '"Join the Serpent King as Family. Together, we shall devour the very Gods!" - Rykard, Lord of Blasphemy',
+    '"I could never betray her, no matter what happens. Ranni, she needs me..." - Blaidd, the Half Wolf',
+    '"I am Malenia, blade of Miquella. And I have never known defeat." - Malenia, blade of Miquella',
+    '"I was able to live as my own person, if only in passing." -Millicent',
+    '"I have given thee courtesy enough. Now I fight as Hoarah Loux, WARRIOR!" -Hoarah Loux',
+    '"Well then, shall we? My dear consort eternal." -Ranni the Witch',
+    '"Have it writ upon thy meagre grave: Felled by Morgott, last of all kings!" -Morgott the Omen King'
 ]
 
 //generates random quote
@@ -21,13 +72,10 @@ function newQuote() {
     const buttons = document.querySelectorAll('.counterButton')
     let count = 0;
 
-    //add event listeners and functionality to each button (no negative counter button 
-    //needed but wanted to include if/else statement)
+    //add event listeners and functionality to each button
     buttons.forEach(function(button){
         button.addEventListener('click', function(){
-            if (button.classList.contains('noReason')) {
-                count--
-            } else if (button.classList.contains('counterButton')){
+            if (button.classList.contains('counterButton')) {
                 count++
             }
 
